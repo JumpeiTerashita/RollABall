47ac0e7309e524e6ed36d0701cb9203ab2f5e2f2
Jumpei Terashita <genuineflat@gmail.com>
2018-02-06 11:39:07 +0900

    カメラが水平回転するように変更

--- Assets/_Completed-Game/Scripts/CameraController.cs
+++ Assets/_Completed-Game/Scripts/CameraController.cs
@@ -9,18 +9,49 @@ public class CameraController : MonoBehaviour {
 	// Store a Vector3 offset from the player (a distance to place the camera from the player at all times)
 	private Vector3 offset;
 
-	// At the start of the game..
-	void Start ()
+    /// <summary>
+    /// カメラのrotation初期値
+    /// </summary>
+    private Quaternion defRotation;
+
+    /// <summary>
+    /// 角度変化量
+    /// </summary>
+    private float deltaAngle;
+
+    /// <summary>
+    /// 角度変化速度 調整用の値
+    /// </summary>
+    [SerializeField]
+    private float angleTrim = 100f;
+
+    // At the start of the game..
+    void Start ()
 	{
 		// Create an offset by subtracting the Camera's position from the player's position
 		offset = transform.position - player.transform.position;
+
+        defRotation = transform.rotation;
+
+        deltaAngle = 0f;
 	}
 
 	// After the standard 'Update()' loop runs, and just before each frame is rendered..
 	void LateUpdate ()
 	{
-		// Set the position of the Camera (the game object this script is attached to)
-		// to the player's position, plus the offset amount
-		transform.position = player.transform.position + offset;
-	}
+        // Set the position of the Camera (the game object this script is attached to)
+        // to the player's position, plus the offset amount
+        //! transform.position = player.transform.position + offset;
+
+        // 水平方向の入力から
+        // 角度変化量を計算
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        deltaAngle += moveHorizontal * Time.deltaTime * angleTrim; // 角度変化分
+
+        // ワールドY軸を中心とした回転分の角度*初期角度
+        transform.rotation = Quaternion.AngleAxis(deltaAngle, Vector3.up) * defRotation;
+        // プレイヤー位置からカメラ後方にoffset（初期位置関係）の距離だけ離れた場所
+        transform.position = player.transform.position - transform.forward * offset.magnitude;
+
+    }
 }
