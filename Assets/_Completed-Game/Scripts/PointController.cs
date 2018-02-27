bdf0b993761ef1605644235e6c4b1ea446cbd8ae
Jumpei Terashita <genuineflat@gmail.com>
2018-02-27 16:06:43 +0900

    ツキアカリのミチシルベ

---
+++ Assets/_Completed-Game/Scripts/PointController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointController : MonoBehaviour
+{
+    [SerializeField] Mesh mesh;
+    [SerializeField] Mesh mesh01;
+    [SerializeField] Mesh mesh02;
+
+    void Start()
+    {
+      
+        mesh.SetIndices(mesh.GetIndices(0), MeshTopology.Lines, 0);
+        mesh01.SetIndices(mesh01.GetIndices(0), MeshTopology.Lines, 0);
+        mesh02.SetIndices(mesh02.GetIndices(0), MeshTopology.Lines, 0);
+
+    }
+}
